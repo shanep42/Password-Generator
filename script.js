1fa18987b52c9b6410dbbb033a0dc13e5b4b3a04
Shane <shanep42@gmail.com>
2022-07-31 16:36:00 -0500

    Adds JS for combining arrays into a complete list of character options

--- script.js
+++ script.js
@@ -13,12 +13,13 @@ function writePassword() {
 // Here is one way I think I know how to do this:
 
 // Write a separate array to list each type of character the user might want included in the password (letters, numbers, special characters)
-var lowercase = []
-var uppercase = []
-var numerals = []
-var specialChar = []
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] // I'm 100% sure there is an easier way to do these.
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const specialChar = [/*There has got to be an easier way*/]
 
 // Check with user which of these they'd like included
+
 
 // Stitch each selected list together into one new array, from which the password will be generated.
 var optionsArray = []
