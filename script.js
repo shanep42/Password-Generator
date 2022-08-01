17562f0fe65d03ff42c71a5c8c69b7e3c4f05634
Shane <shanep42@gmail.com>
2022-07-31 19:50:57 -0500

    Replaces browser prompt input with checkboxes and number input, linked with JS

--- script.js
+++ script.js
@@ -7,33 +7,43 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Here is one way I think I know how to do this:
 
 // Write a separate array to list each type of character the user might want included in the password (letters, numbers, special characters)
-const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] // I'm 100% sure there is an easier way to do these.
-const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-const numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-const specialChar = [/*There has got to be an easier way*/]
+function generatePassword() {
+  const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] // I'm 100% sure there is an easier way to do these.
+  const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  const numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  const specialChar = [/*There has got to be an easier way*/]
 
-// Check with user which of these they'd like included
+  // Check with user which of these they'd like included
+  // Stitch each selected list together into one new array, from which the password will be generated.
+  var optionsArray = [];
 
+  if (document.getElementById('lowercase').checked) {
+    optionsArray = optionsArray.concat(lowercase);
+  }
 
-// Stitch each selected list together into one new array, from which the password will be generated.
-var optionsArray = []
-// Have user specify how long they would like the generated password to be
-var passwordLength = 0 // TODO: Decide how we're getting user input.
+  if (document.getElementById('uppercase').checked) {
+    optionsArray = optionsArray.concat(uppercase)
+  }
 
-// Pick characters from the combined character option array by randomly selecting indices until the password is the requested length.
+  if (document.getElementById('numerals').checked) {
+    optionsArray = optionsArray.concat(specialChar)
+  }
 
-while (password.length < passwordLength) {
-  password = password + optionsArray[Math.floor(Math.random * (optionsArray.length + 1))]
+  var passwordLength = document.getElementById('length').value// TODO: Decide how we're getting user input.
+  console.log(`The password length has been set to ${passwordLength}`)
+
+  // Pick characters from the combined character option array by randomly selecting indices until the password is the requested length.
+
+  while (password.length < passwordLength) {
+    password = password + optionsArray[Math.floor(Math.random * (optionsArray.length + 1))]
+  }
 }
 
-// Print that to an alert.
-alert(`${password}`)
 
 
 // Add event listener to generate button
