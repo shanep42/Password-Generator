3b44a816f7025b379bb4c09ce5dfbce011bc8f16
Shane <shanep42@gmail.com>
2022-08-01 21:55:53 -0500

    Adds new console logs for troubleshooting purposes

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
 }
 
 // Here is one way I think I know how to do this:
@@ -16,8 +16,7 @@ function generatePassword() {
   const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] // I'm 100% sure there is an easier way to do these.
   const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   const numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  const specialChar = [/*There has got to be an easier way*/]
-
+  const specialChar = []
   // Check with user which of these they'd like included
   // Stitch each selected list together into one new array, from which the password will be generated.
   var optionsArray = [];
@@ -34,7 +33,7 @@ function generatePassword() {
     optionsArray = optionsArray.concat(specialChar)
   }
 
-  var passwordLength = document.getElementById('length').value// TODO: Decide how we're getting user input.
+  var passwordLength = document.getElementById('length').value
   console.log(`The password length has been set to ${passwordLength}`)
 
   // Pick characters from the combined character option array by randomly selecting indices until the password is the requested length.
@@ -42,6 +41,8 @@ function generatePassword() {
   while (password.length < passwordLength) {
     password = password + optionsArray[Math.floor(Math.random * (optionsArray.length + 1))]
   }
+  console.log(password)
+  return password;
 }
 
 
